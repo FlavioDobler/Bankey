957e35e68275d3608de39e6ae1341a59c489dfb9
Flavio Dobler <flaviodobler@Flavios-MacBook-Air.local>
2023-07-08 01:24:47 -0300

    Added first onboarding screen

--- Bankey/Bankey/SceneDelegate.swift
+++ Bankey/Bankey/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = OnboardingContainerViewController()
+        let vc = OnboardingViewController()
         let nav = UINavigationController(rootViewController: vc)
         window.rootViewController = nav
         window.makeKeyAndVisible()
